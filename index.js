09c342c9db23b52b809729a8a4cf44eec9d61763
Khoutsaneh Kettavong <laekettavong@Khoutsanehs-iMac.local>
2018-09-06 00:34:11 -0400

    Added CLI functionality

--- index.js
+++ index.js
@@ -5,12 +5,18 @@
 // Dependencies
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli');
 
 // Declare the app
 let app = {
     init : () => {
         server.init();
         workers.init();
+
+        // Start te CLI, but make sure it starts last
+        setTimeout(() => {
+            cli.init();
+        }, 50);
     }
 }
 
